e25289aacb35ed7f5439f2754f83117b3ad432bc
Rikotee <teerek81@gmail.com>
2020-11-16 13:11:38 +0200

    more text

--- collabTest.js
+++ collabTest.js
@@ -2,9 +2,14 @@
 
 console.log('Git test');
 
-//git commands:
+//GIT COMMANDS:
 
 // git status
+
+// git remote add origin https://github.com/OskarWiiala/Smartsoft.git
+// git branch -M main
+// git push -u origin main
+
 
 // git add .
 // git commit -m "text"
