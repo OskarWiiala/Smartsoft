8d85e3425efc8f603dfac5da7ac671831bfeb257
Rikotee <teerek81@gmail.com>
2020-11-16 13:16:37 +0200

    git commands text2

--- collabTest.js
+++ collabTest.js
@@ -4,13 +4,18 @@ console.log('Git test');
 
 //GIT COMMANDS:
 
+// git clone <url> 					        # Clone directory
+// git checkout -b <new-branch> 		# Create new local branch
+// git push -u origin <new-branch> 	# Sync local branch with remote
+// git checkout <branch> 				    # Checkout branch
+// git push origin <branch> 			  # Push branch to remote
 // git status
+
 
 // git remote add origin https://github.com/OskarWiiala/Smartsoft.git
 // git branch -M main
 // git push -u origin main
 
 
-// git add .
-// git commit -m "text"
-// git push -u origin main
+
+
