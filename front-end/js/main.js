f7c13632d299fa72ed4af5505349fa9ef6641df6
Rikotee <teerek81@gmail.com>
2020-11-25 16:24:26 +0200

    main.js 25.11

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -3,28 +3,52 @@ const url = 'http://localhost:3000'; // change url when uploading to server
 
 const ul = document.querySelector('ul');
 
-const getFoodPost = async () => {
-  const response = await fetch(url + '/foodPost');
-  const foodPosts = await response.json();
-  for (const foodPost of foodPosts) {
-    const user = await getUser(foodPost.user);
-    ul.innerHTML += `
-    <li>
-        <h2>${foodPost.title}</h2>
-        <figure>
-            <img src="${foodPost.filename}" class="resp">
-        </figure>
-        <p>Title: ${foodPost.title}</p>
-        <p>Recipe: ${foodPost.text}kg</p>
-    </li>
-    `;
-  }
+// // select existing html elements
+// const addForm = document.querySelector('#addCatForm');
+// const modForm = document.querySelector('#modCatForm');
+const ul = document.querySelector('ul');
+// const userLists = document.querySelectorAll('.add-owner');
+
+// create recipe cards
+const createFoodPostCards = (recipes) => {
+  // clear ul
+  ul.innerHTML = '';
+  recipes.forEach((foodPost) => {
+    // create li with DOM methods
+    const img = document.createElement('img');
+    img.src = url + '/' + foodPost.filename;
+    img.alt = foodPost.title;
+    img.classList.add('resp');
+
+    const figure = document.createElement('figure').appendChild(img);
+
+    const h2 = document.createElement('h2');
+    h2.innerHTML = foodPost.title;
+
+    const p1 = document.createElement('p');
+    p1.innerHTML = `recipe: ${foodPost.text}`;
+
+
+    const li = document.createElement('li');
+    li.classList.add('light-border');
+
+    li.appendChild(h2);
+    li.appendChild(figure);
+    li.appendChild(p1);
+    ul.appendChild(li);
+  });
 };
 
-const getUser = async (id) => {
-  const response = await fetch(url + '/user/' + id);
-  const user = await response.json();
-  return user;
+// AJAX call
+const getFoodPost = async () => {
+  try {
+    const response = await fetch(url + '/foodPost');
+    const recipes = await response.json();
+    createFoodPostCards(recipes);
+  }
+  catch (e) {
+    console.log(e.message);
+  }
 };
 
 getFoodPost();
