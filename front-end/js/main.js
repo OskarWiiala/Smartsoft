57e3a8b90625056012448135ea1eb3819eda14b8
TuomasB73 <tuomasbergholm@gmail.com>
2020-12-07 17:09:32 +0200

    Added the my posts section to my profile

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -8,7 +8,8 @@ const loginWrapper = document.querySelector('#loginWrapper');
 const loginForm = document.querySelector('#login-form');
 const logOut = document.querySelector('#log-out');
 const userInfo = document.querySelector('#user-info');
-const ProfilePge = document.querySelector('#profilePage');
+const profilePageButton = document.querySelector('#profilePageButton');
+const homePageButton = document.querySelector('#homePageButton');
 const addLoginFormButton = document.querySelector('#addLoginFormButton');
 const addUserPage = document.querySelector('#addUserPage');
 const addUserForm = document.querySelector('#add-user-form');
@@ -34,6 +35,7 @@ const addLikesForm = document.querySelector('#add-like-form');
 const imageModal = document.querySelector('#image-modal');
 const modalImage = document.querySelector('#image-modal img');
 const close = document.querySelector('#image-modal a');
+const myPostsHeader = document.querySelector('#MyPostsHeader');
 
 let loggedInUserId = null;
 let loggedInUserStatus = null;
@@ -42,180 +44,198 @@ let loggedInUserStatus = null;
 const createFoodPostCards = (recipes) => {
   // clear ul
   ul.innerHTML = '';
-  recipes.forEach((foodPost) => {
-    if (foodPost.status === 'public') {
-      // create li with DOM methods
-      const img = document.createElement('img');
-      img.src = url + '/thumbnails/' + foodPost.filename;
-      img.alt = foodPost.title;
-      img.classList.add('resp');
-
-      // open large image when clicking image
-      img.addEventListener('click', () => {
-        modalImage.src = url + '/' + foodPost.filename;
-        imageModal.alt = foodPost.title;
-        imageModal.classList.toggle('hide');
-      });
-
-      const figure = document.createElement('figure').appendChild(img);
-
-      const user = document.createElement('h4');
-      user.innerHTML = foodPost.username;
-      user.classList.add('cardUserHeader');
-
-      const h2 = document.createElement('h2');
-      h2.innerHTML = foodPost.title;
-      h2.classList.add('cardh2');
-
-      const p1 = document.createElement('textarea');
-      p1.innerHTML = `Recipe: ${foodPost.text}`;
-      p1.classList.add('cardRecipe');
-      p1.readOnly = true;
-
-      const likes = document.createElement('likes');
-      likes.innerHTML = `${foodPost.likes}`;
-
-      const dislikes = document.createElement('dislikes');
-      dislikes.innerHTML = `${foodPost.dislikes}`;
-
-      const clnU = iconU.cloneNode(true);
-      const clnD = iconD.cloneNode(true);
-
-      const card = document.createElement('card');
-      card.classList.add('roundEdge');
-      card.classList.add('cardImage');
-      card.appendChild(user);
-      card.appendChild(h2);
-      card.appendChild(figure);
-      card.appendChild(p1);
-      card.appendChild(clnU);
-      card.appendChild(likes);
-      card.appendChild(clnD);
-      card.appendChild(dislikes);
-      ul.appendChild(card);
-
-      // if the logged in user id matches the foodPost user id, or its status is
-      // admin, the delete and modify buttons will be created
-      if (loggedInUserId === foodPost.user || loggedInUserStatus === 'admin') {
-        // delete selected foodPost
-        const delButton = document.createElement('button');
-        delButton.innerHTML = 'Delete';
-        delButton.classList.add('cardButton');
-
-        delButton.addEventListener('click', async () => {
-          if(confirm(`Are you sure you want to delete "${foodPost.title}"`)) {
-          const fetchOptions = {
-            method: 'DELETE',
-            headers: {
-              'Authorization': 'Bearer ' + sessionStorage.getItem('token'),
-            },
-          };
-          try {
-            const response = await fetch(
-                url + '/foodPost/' + foodPost.food_post_id,
-                fetchOptions);
-            const json = await response.json();
-            console.log('delete response', json);
-            getFoodPost();
-          } catch (e) {
-            console.log(e.message);
-          }
-        }});
-
-        card.appendChild(delButton);
-
-        // modify selected foodPost
-        const modButton = document.createElement('button');
-        modButton.innerHTML = 'Modify';
-        modButton.classList.add('cardButton');
-
-        modButton.addEventListener('click', () => {
-          const inputs = modifyFoodPostForm.querySelectorAll('input');
-          inputs[0].value = foodPost.title;
-          // inputs[1].value = foodPost.text;
-          modifyTextarea.innerHTML = foodPost.text;
-          inputs[1].value = foodPost.food_post_id;
-          inputs[2].value = foodPost.status;
-          if (foodPost.status === 'private') {
-            modifyPostCheckBox.checked = true;
-          }
-          modifyContainer.style.display = 'flex';
-
-          //This scrolls the page to the top
-          window.scroll({
-            top: 0,
-            left: 0,
-            behavior: 'smooth',
-          });
-        });
-        card.appendChild(modButton);
+  // if the my profile button is not displayed, meaning the my profile page is
+  // opened, only the user's own food posts are displayed
+  if (profilePageButton.style.display === 'none' && loggedInUserId != null) {
+    recipes.forEach((foodPost) => {
+      if (loggedInUserId === foodPost.user) {
+        createCardContent(foodPost);
       }
+    });
+  } else {
+    recipes.forEach((foodPost) => {
+      if (foodPost.status === 'public') {
+        createCardContent(foodPost);
+      }
+    });
+  }
+};
 
-      // Adds one like to ss_rating
-      clnU.addEventListener('click', async (evt) => {
+const createCardContent = async (foodPost) => {
+  // create li with DOM methods
+  const img = document.createElement('img');
+  img.src = url + '/thumbnails/' + foodPost.filename;
+  img.alt = foodPost.title;
+  img.classList.add('resp');
 
-        // This disables button
-        iconU.disabled = true;
-        console.log('iconU is disabled');
-
-        const likes = foodPost.likes;
-        const inputs = addLikesForm.querySelectorAll('input');
-        inputs[0].value = foodPost.food_post_id;
-        inputs[1].value = likes + 1;
-        inputs[2].value = foodPost.dislikes;
-
-        const data = serializeJson(addLikesForm);
-        console.log('modify rating func after add', data);
-
-        const fetchOptions = {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + sessionStorage.getItem('token'),
-          },
-          body: JSON.stringify(data),
-        };
-        const response = await fetch(url + '/rating', fetchOptions);
-        const json = await response;
-        console.log('add 1 like rating response', json);
-        await getFoodPost();
-        addLikesForm.reset();
-        await buttonDisabler();
-      });
-
-      // Adds one dislike to ss_rating
-      clnD.addEventListener('click', async (evt) => {
-
-        // This disables button
-        iconD.disabled = true;
-        console.log('iconD is disabled');
-
-        const addDislike = foodPost.dislikes + 1;
-        const inputs = addLikesForm.querySelectorAll('input');
-        inputs[0].value = foodPost.food_post_id;
-        inputs[1].value = foodPost.likes;
-        inputs[2].value = addDislike;
-
-        const data = serializeJson(addLikesForm);
-        console.log('modify rating func after add', data);
-
-        const fetchOptions = {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + sessionStorage.getItem('token'),
-          },
-          body: JSON.stringify(data),
-        };
-        const response = await fetch(url + '/rating', fetchOptions);
-        const json = await response;
-        console.log('add 1 like rating response', json);
-        await getFoodPost();
-        addLikesForm.reset();
-        await buttonDisabler();
-      });
-    }
+  // open large image when clicking image
+  img.addEventListener('click', () => {
+    modalImage.src = url + '/' + foodPost.filename;
+    imageModal.alt = foodPost.title;
+    imageModal.classList.toggle('hide');
   });
+
+  const figure = document.createElement('figure').appendChild(img);
+
+  const user = document.createElement('h4');
+  user.innerHTML = foodPost.username;
+  user.classList.add('cardUserHeader');
+
+  const h2 = document.createElement('h2');
+  h2.innerHTML = foodPost.title;
+  h2.classList.add('cardh2');
+
+  const p1 = document.createElement('textarea');
+  p1.innerHTML = `Recipe: ${foodPost.text}`;
+  p1.classList.add('cardRecipe');
+  p1.readOnly = true;
+
+  const likes = document.createElement('likes');
+  likes.innerHTML = `${foodPost.likes}`;
+
+  const dislikes = document.createElement('dislikes');
+  dislikes.innerHTML = `${foodPost.dislikes}`;
+
+  const clnU = iconU.cloneNode(true);
+  const clnD = iconD.cloneNode(true);
+
+  const card = document.createElement('card');
+  card.classList.add('roundEdge');
+  card.classList.add('cardImage');
+  card.appendChild(user);
+  card.appendChild(h2);
+  card.appendChild(figure);
+  card.appendChild(p1);
+  card.appendChild(clnU);
+  card.appendChild(likes);
+  card.appendChild(clnD);
+  card.appendChild(dislikes);
+  ul.appendChild(card);
+
+  // Adds one like to ss_rating
+  clnU.addEventListener('click', async (evt) => {
+
+    // This disables button
+    iconU.disabled = true;
+    console.log('iconU is disabled');
+
+    const likes = foodPost.likes;
+    const inputs = addLikesForm.querySelectorAll('input');
+    inputs[0].value = foodPost.food_post_id;
+    inputs[1].value = likes + 1;
+    inputs[2].value = foodPost.dislikes;
+
+    const data = serializeJson(addLikesForm);
+    console.log('modify rating func after add', data);
+
+    const fetchOptions = {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + sessionStorage.getItem('token'),
+      },
+      body: JSON.stringify(data),
+    };
+    const response = await fetch(url + '/rating', fetchOptions);
+    const json = await response;
+    console.log('add 1 like rating response', json);
+    await getFoodPost();
+    addLikesForm.reset();
+    await buttonDisabler();
+  });
+
+  // Adds one dislike to ss_rating
+  clnD.addEventListener('click', async (evt) => {
+
+    // This disables button
+    iconD.disabled = true;
+    console.log('iconD is disabled');
+
+    const addDislike = foodPost.dislikes + 1;
+    const inputs = addLikesForm.querySelectorAll('input');
+    inputs[0].value = foodPost.food_post_id;
+    inputs[1].value = foodPost.likes;
+    inputs[2].value = addDislike;
+
+    const data = serializeJson(addLikesForm);
+    console.log('modify rating func after add', data);
+
+    const fetchOptions = {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + sessionStorage.getItem('token'),
+      },
+      body: JSON.stringify(data),
+    };
+    const response = await fetch(url + '/rating', fetchOptions);
+    const json = await response;
+    console.log('add 1 like rating response', json);
+    await getFoodPost();
+    addLikesForm.reset();
+    await buttonDisabler();
+  });
+
+  // if the logged in user id matches the foodPost user id, or the logged in user's
+  // status is admin, the delete and modify buttons will be created
+  if (loggedInUserId === foodPost.user || loggedInUserStatus === 'admin') {
+    await createDeleteModifyButtons(foodPost, card);
+  }
+};
+
+const createDeleteModifyButtons = async (foodPost, card) => {
+  // delete selected foodPost
+  const delButton = document.createElement('button');
+  delButton.innerHTML = 'Delete';
+  delButton.classList.add('cardButton');
+
+  delButton.addEventListener('click', async () => {
+    if(confirm(`Are you sure you want to delete "${foodPost.title}"`)) {
+      const fetchOptions = {
+        method: 'DELETE',
+        headers: {
+          'Authorization': 'Bearer ' + sessionStorage.getItem('token'),
+        },
+      };
+      try {
+        const response = await fetch(
+            url + '/foodPost/' + foodPost.food_post_id,
+            fetchOptions);
+        const json = await response.json();
+        console.log('delete response', json);
+        await getFoodPost();
+      } catch (e) {
+        console.log(e.message);
+      }
+    }});
+
+  card.appendChild(delButton);
+
+  // modify selected foodPost
+  const modButton = document.createElement('button');
+  modButton.innerHTML = 'Modify';
+  modButton.classList.add('cardButton');
+
+  modButton.addEventListener('click', () => {
+    const inputs = modifyFoodPostForm.querySelectorAll('input');
+    inputs[0].value = foodPost.title;
+    // inputs[1].value = foodPost.text;
+    modifyTextarea.innerHTML = foodPost.text;
+    inputs[1].value = foodPost.food_post_id;
+    inputs[2].value = foodPost.status;
+    if (foodPost.status === 'private') {
+      modifyPostCheckBox.checked = true;
+    }
+    modifyContainer.style.display = 'flex';
+
+    //This scrolls the page to the top
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  });
+  card.appendChild(modButton);
 };
 
 // returns disabled icon buttons back to work
@@ -255,6 +275,7 @@ const getFoodPost = async () => {
     console.log(e.message);
   }
 };
+
 getFoodPost();
 
 // login event listener
@@ -284,7 +305,7 @@ const login = async () => {
     sessionStorage.setItem('token', json.token);
     loginFormContainer.style.display = 'none';
     logOut.style.display = 'block';
-    ProfilePge.style.display = 'block';
+    profilePageButton.style.display = 'block';
     addUserPage.style.display = 'none';
     loggedInUserId = json.user.user_id;
     loggedInUserStatus = json.user.status;
@@ -316,15 +337,17 @@ logOut.addEventListener('click', async (evt) => {
     // loginForm.style.display = 'block';
     logOut.style.display = 'none';
     userInfo.innerHTML = ``;
-    ProfilePge.style.display = 'none';
+    profilePageButton.style.display = 'none';
     addPost.style.display = 'none';
     addUserPage.style.display = 'block';
     addLoginFormButton.style.display = 'block';
     postContainer.style.display = 'none';
     modifyContainer.style.display = 'none';
+    myPostsHeader.style.display = 'none';
+    homePageButton.style.display = 'none';
     loggedInUserId = null;
     loggedInUserStatus = null;
-    getFoodPost();
+    await getFoodPost();
   } catch (e) {
     console.log(e.message);
   }
@@ -392,11 +415,24 @@ addUserForm.addEventListener('submit', async (evt) => {
   addUserForm.reset();
 });
 
-ProfilePge.addEventListener('click', async (evt) => {
+// when the my profile button is pressed, the my profile content appears
+profilePageButton.addEventListener('click', async (evt) => {
   evt.preventDefault();
   addPost.style.display = 'block';
-  cancelPost.style.display = 'block';
-  // addForm.style.display = 'flex';
+  profilePageButton.style.display = 'none';
+  homePageButton.style.display = 'block';
+  myPostsHeader.style.display = 'block';
+  await getFoodPost();
+});
+
+// when the home page button is pressed, it disappears and the my profile button appears
+homePageButton.addEventListener('click', async (evt) => {
+  evt.preventDefault();
+  homePageButton.style.display = 'none';
+  profilePageButton.style.display = 'block';
+  addPost.style.display = 'none';
+  myPostsHeader.style.display = 'none';
+  await getFoodPost();
 });
 
 //Used to display post-container when clicking "create new post" button in the navigation
@@ -460,7 +496,7 @@ addForm.addEventListener('submit', async (evt) => {
   postContainer.style.display = 'none';
   addPost.style.display = 'block';
   await addRating(json.food_post_id);
-  getFoodPost();
+  await getFoodPost();
   addForm.reset();
 });
 
