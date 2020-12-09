dbccea4138d544c837a860f9a884ff82f305b339
TuomasB73 <tuomasbergholm@gmail.com>
2020-12-09 16:25:02 +0200

    https added

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const ratingRoute = require('./routes/ratingRoute');
 const app = express();
 const port = 3000;
 
-
 app.use(cors());
 
 app.use(express.json()); // for parsing application/json
@@ -21,6 +20,13 @@ app.use(express.urlencoded({extended: true})); // for parsing application/x-www-
 app.use(express.static('uploads'));
 app.use('/thumbnails', express.static('thumbnails'));
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+if (process.env.NODE_ENV === 'production') {
+  require('./production')(app, process.env.PORT);
+} else {
+  require('./localhost')(app, process.env.HTTPS_PORT, process.env.HTTP_PORT);
+}
+
 // routes
 app.use('/', rootRoute);
 app.use('/auth', authRoute);
@@ -28,6 +34,4 @@ app.use('/foodPost', foodPostRoute);
 app.use('/rating', ratingRoute);
 app.use('/user', passport.authenticate('jwt', {session: false}), userRoute);
 
-
-
-app.listen(port, () => console.log(`Smartsoft web app listening on port ${port}!`));
+//app.listen(port, () => console.log(`Smartsoft web app listening on port ${port}!`));
