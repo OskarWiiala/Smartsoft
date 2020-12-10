a4473bcccf7ab1931ecf344cab00a6ce5904958b
OskarWiiala <pullumastery@gmail.com>
2020-12-10 17:14:30 +0200

    commented eventlistener for show menu/hide menu

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -571,6 +571,7 @@ cancelPost.addEventListener('click', async () => {
   addForm.reset();
 });
 
+// This is for the hide menu/show menu button in the sticky navigation section on top of the page
 toggleMiddleButton.addEventListener('click', async () => {
   if (middle.style.display !== 'none') {
     middle.style.display = 'none';
